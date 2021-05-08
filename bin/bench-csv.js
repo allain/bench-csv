a7adfd56ac4e31aea9ecf31eb42398aaf779dbe6
allain <allain.lalonde@gmail.com>
2021-05-08 18:11:27 -0400

    getting tests runnign again

--- bin/bench-csv.js
+++ bin/bench-csv.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node --expose-gc
 
-const path = require('path')
-const { promisify } = require('util')
-const buildCommand = require('../src/build-command')
-const nodemon = require('nodemon')
-const fs = require('fs-extra')
+const path = require("path");
+const { promisify } = require("util");
+const buildCommand = require("../src/build-command");
+const nodemon = require("nodemon");
+const fs = require("fs-extra");
 
-const delay = ms =>
-  new Promise(resolve => setTimeout(() => resolve(Date.now()), ms))
+const delay = (ms) =>
+  new Promise((resolve) => setTimeout(() => resolve(Date.now()), ms));
 
-const bm = require('../')
-const buildCsv = require('../src/build-csv')
+const bm = require("../");
+const buildCsv = require("../src/build-csv");
 
-process.on('SIGINT', () => process.exit(0))
-process.on('SIGTERM', () => process.exit(0))
-process.on('SIGUSR2', () => process.exit(0))
+process.on("SIGINT", () => process.exit(0));
+process.on("SIGTERM", () => process.exit(0));
+process.on("SIGUSR2", () => process.exit(0));
 
-const args = require('minimist')(process.argv, {
-  boolean: ['header', 'watch', 'watching', 'inspect'],
+const args = require("minimist")(process.argv, {
+  boolean: ["header", "watch", "watching", "inspect"],
   default: {
     delay: 1,
     header: true,
@@ -26,101 +26,102 @@ const args = require('minimist')(process.argv, {
     inspect: false,
     warmups: 0,
     repeat: 1,
-    delim: ','
-  }
-})
+    delim: ",",
+  },
+});
 
-async function prepareOptions (cmdLineOptions) {
-  let target = cmdLineOptions._[2]
+async function prepareOptions(cmdLineOptions) {
+  let target = cmdLineOptions._[2];
   if (!target) {
-    console.error('no target specified')
-    console.error('usage: bench-csv target [options]')
-    process.exit(1)
+    console.error("no target specified");
+    console.error("usage: bench-csv target [options]");
+    process.exit(1);
   }
 
-  let targetPath = path.resolve(process.cwd(), target)
-  if (!await fs.exists(targetPath)) throw new Error(`not found: ${targetPath}`)
+  let targetPath = path.resolve(process.cwd(), target);
+  if (!(await fs.exists(targetPath)))
+    throw new Error(`not found: ${targetPath}`);
 
-  const targetSpec = require(targetPath)
-  let options
-  if (typeof targetSpec === 'function') {
-    options = cmdLineOptions
-    target = targetSpec
-  } else if (!Array.isArray(targetSpec) && typeof targetSpec === 'object') {
-    options = Object.assign({}, cmdLineOptions, targetSpec)
-    target = options.run
+  const targetSpec = require(targetPath);
+  let options;
+  if (typeof targetSpec === "function") {
+    options = cmdLineOptions;
+    target = targetSpec;
+  } else if (!Array.isArray(targetSpec) && typeof targetSpec === "object") {
+    options = Object.assign({}, cmdLineOptions, targetSpec);
+    target = options.run;
   } else {
-    throw new Error('target did not export a function or object')
+    throw new Error("target did not export a function or object");
   }
 
   if (!target) {
     throw new Error(
       'function to benchmark not found in target, did you forget "run" property?'
-    )
+    );
   }
 
-  options.targetPath = targetPath
+  options.targetPath = targetPath;
 
-  return { target, options }
+  return { target, options };
 }
 
-async function emitStats (stats, options) {
-  const delim = JSON.parse('"' + options.delim + '"')
+async function emitStats(stats, options) {
+  const delim = JSON.parse('"' + options.delim + '"');
   const emitHeader =
-    options.header && (!options.o || !await fs.exists(options.o))
+    options.header && (!options.o || !(await fs.exists(options.o)));
   if (options.o) {
     // Write to file
-    const outputPath = path.resolve(process.cwd(), options.o)
-    if (!await fs.exists(outputPath)) {
-      await fs.mkdirp(path.dirname(outputPath))
+    const outputPath = path.resolve(process.cwd(), options.o);
+    if (!(await fs.exists(outputPath))) {
+      await fs.mkdirp(path.dirname(outputPath));
     }
 
     await fs.appendFile(
       outputPath,
-      buildCsv(stats, delim, emitHeader) + '\n',
-      'utf-8'
-    )
+      buildCsv(stats, delim, emitHeader) + "\n",
+      "utf-8"
+    );
   } else {
-    console.log(buildCsv(stats, delim, emitHeader))
+    console.log(buildCsv(stats, delim, emitHeader));
   }
 }
 
 prepareOptions(args)
   .then(async ({ target, options }) => {
-    let i = 0
+    let i = 0;
     do {
-      let stats = await bm(target, options)
-      await emitStats(stats, options)
-      options.header = false
-      options.warmups = false // After first run when being watched no need to run warmups between
-    } while (options.watching && (await delay(options.delay * 1000)))
+      let stats = await bm(target, options);
+      await emitStats(stats, options);
+      options.header = false;
+      options.warmups = false; // After first run when being watched no need to run warmups between
+    } while (options.watching && (await delay(options.delay * 1000)));
 
     if (options.watch) {
-      await delay(options.delay * 1000)
+      await delay(options.delay * 1000);
 
       let newOptions = {
         ...options,
         _: [path.relative(process.cwd(), __filename), options.targetPath],
         header: false,
         watch: false,
-        watching: true // so that children know they're being watched and wil lrun forever
-      }
+        watching: true, // so that children know they're being watched and wil lrun forever
+      };
 
-      const nodeOptions = { 'expose-gc': true }
+      const nodeOptions = { "expose-gc": true };
       for (let opt of [
-        'inspect',
-        'trace-opt',
-        'trace-deopt',
-        'trace-opt-verbose'
+        "inspect",
+        "trace-opt",
+        "trace-deopt",
+        "trace-opt-verbose",
       ]) {
         if (options[opt]) {
-          nodeOptions[opt] = true
+          nodeOptions[opt] = true;
         }
       }
-      nodemon(buildCommand(nodeOptions) + ' ' + buildCommand(newOptions))
+      nodemon(buildCommand(nodeOptions) + " " + buildCommand(newOptions));
     }
   })
-  .catch(e => {
-    console.error(e)
-    process.exit(1)
-  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
--- bin/bench-csv.test.js
+++ bin/bench-csv.test.js
@@ -1,16 +1,16 @@
-const path = require('path')
-const childProcess = require('child_process')
+const path = require("path");
+const childProcess = require("child_process");
 
 // const cli = require('./bench-csv')
-const cliPath = path.resolve(__dirname, './bench-csv.js')
+const cliPath = path.resolve(__dirname, "./bench-csv.js");
 
-const pathToTest = path.resolve(__dirname, '../examples/random.js')
+const pathToTest = path.resolve(__dirname, "../examples/random.js");
 
-const run = cmd => childProcess.execSync(cmd)
+const run = (cmd) => childProcess.execSync(cmd);
 
-describe('cli', () => {
-  it('can be invoked as a command', () => {
-    const result = run(`${cliPath} ${pathToTest}`)
-    expect(result).toBeTruthy()
-  })
-})
+describe("cli", () => {
+  it("can be invoked as a command", () => {
+    const result = run(`node --expose-gc ${cliPath} ${pathToTest}`);
+    expect(result).toBeTruthy();
+  });
+});
